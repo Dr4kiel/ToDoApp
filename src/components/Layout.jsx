d8fd56aa9988c9640d3be666d8094672f0a40463
Dr4kiel <tristangautier2003@gmail.com>
2023-04-18 12:08:44 +0200

    Update BootStrap

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,24 +4,6 @@ export default function Layout() {
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-light">
-                <div className="container-fluid">
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/">Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="addTask">Add Task</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="taskList">Task List</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-
             <Outlet />
         </>
     )
